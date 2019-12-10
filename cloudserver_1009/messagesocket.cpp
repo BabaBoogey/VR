1f0ff83ec26e89228078cbf22066b0d3963d61ef
WHM-GITHUB <2855270738@users.noreply.github.com>
2019-12-11 01:07:20 +0800

    Update messagesocket.cpp

--- cloudserver_1009/messagesocket.cpp
+++ cloudserver_1009/messagesocket.cpp
@@ -68,18 +68,22 @@ void MessageSocket::MessageSocketSlot_Read()
                 resindexProcess(ResMsg);
             }else if(segmentRex.indexIn(msg)!=-1)
             {
+                qDebug()<<msg;
                 QString seg=segmentRex.cap(1).trimmed();
                 segProcess(seg);
             }else if(deleteRex.indexIn(msg)!=-1)
             {
+//                qDebug()<<msg;
                 QString delcurvepos=deleteRex.cap(1).trimmed();
                 deleteProcess(delcurvepos);
             }else if(markerRex.indexIn(msg)!=-1)
             {
+//                qDebug()<<msg;
                 QString markerpos=markerRex.cap(1).trimmed();
                 markerProcess(markerpos);
             }else if(delmarkerRex.indexIn(msg)!=-1)
             {
+//                qDebug()<<msg;
                 QString delmarkerpos=delmarkerRex.cap(1).trimmed();
                 delmaekerProcess(delmarkerpos);
             }else if(scaleRex.indexIn(msg)!=-1)
